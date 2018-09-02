1524695357077180eb9379d7405794b64740b0fe
Shuichiro Fukuoka <syumai@gmail.com>
2018-09-03 03:23:35 +0900

    Add first implementation of Component

--- examples/example.go
+++ examples/example.go
@@ -6,12 +6,22 @@ import (
 	h "github.com/syumai/go-hyperscript"
 )
 
+func List(names ...string) h.Component {
+	elements := make(h.Elements, len(names))
+	for i, name := range names {
+		elements[i] = h.H("li", nil, h.TextNode(name))
+	}
+	return func() h.Element { return h.H("ul", nil, elements...) }
+}
+
 func main() {
 	node := h.H("div", nil,
 		h.H("h1", nil, h.TextNode("Example App")),
 		h.H("strong", nil,
 			h.H("font", h.Object{"color": "red"}, h.TextNode("Hello, world!")),
 		),
+		h.H(List("a", "b", "c"), nil),
+		h.H(List("d", "e", "f"), nil),
 	)
 	fmt.Println(node.ToString())
 }
--- hyperscript.go
+++ hyperscript.go
@@ -29,11 +29,50 @@ func (elements Elements) ToString() string {
 	return str
 }
 
-func H(tag string, attrs Object, elements ...Element) Element {
+type (
+	Component func() Element
+)
+
+func mergeElements(a Elements, b Elements) Elements {
+	lenA := len(a)
+	ary := make(Elements, lenA+len(b))
+	for i, v := range a {
+		ary[i] = v
+	}
+	for i, v := range b {
+		ary[i+lenA] = v
+	}
+	return ary
+}
+
+func mergeObjects(a Object, b Object) Object {
+	obj := Object{}
+	for k, v := range a {
+		obj[k] = v
+	}
+	for k, v := range b {
+		obj[k] = v
+	}
+	return obj
+}
+
+func H(tag interface{}, attrs Object, children ...Element) Element {
+	if c, ok := tag.(Component); ok {
+		node := c()
+		if n, ok := node.(*Node); ok {
+			n.Attributes = mergeObjects(n.Attributes, attrs)
+			n.Children = mergeElements(n.Children, children)
+		}
+		return node
+	}
+	s, ok := tag.(string)
+	if !ok {
+		return TextNode("")
+	}
 	return &Node{
-		NodeName:   tag,
+		NodeName:   s,
 		NodeType:   NODE_TYPE_ELEMENT_NODE,
-		Children:   elements,
+		Children:   children,
 		Attributes: attrs,
 	}
 }
